1bdb662a3539c774e788fd4f694af40d5c6cb123
wer-cyber <117658310+wer-cyber@users.noreply.github.com>
2022-11-07 16:18:23 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTaY_P9MKReZIJFObxsZpnNULBMQRi3qO_y1DypePhPVHIWhzWCGllWqMFbbnDGrD4fM5SLU-fUYsiu/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1fYoREBL2-ghD740tXclJHSEtiHoDbE8BNM5j4Q7JwdU/edit#gid=1033046795';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
